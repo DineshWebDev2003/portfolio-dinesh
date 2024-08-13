81b050bd486ff310ebf24e8a46f4960b63d12461
DineshWebDev2003 <dineshmahi02@gmail.com>
2024-08-13 19:23:22 +0530

    change nav

--- script.js
+++ script.js
@@ -5,13 +5,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Show navigation menu
     hamburger.addEventListener('click', function () {
-        navLinks.classList.add('active');
+        navLinks.classList.toggle('active');
+       
     });
 
     // Hide navigation menu
-    closeIcon.addEventListener('click', function () {
+    navLinks.addEventListener('click', function () {
         navLinks.classList.remove('active');
     });
+
+
+
+    
 
     // Hide navigation menu when clicking outside of it (optional enhancement)
     document.addEventListener('click', function (event) {
